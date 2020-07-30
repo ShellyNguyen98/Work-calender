423397b2719885710e34fd420b000954600fc0a9
unknown <Shellynguyen98@gmail.com>
2020-07-30 15:26:22 -0700

    update

--- time.js
+++ time.js
@@ -1,44 +1,81 @@
-$('#currentDay').text(`
-    ${moment().format('MMMM Do YYYY')}
-`)
+// $('#currentDay').text(`
+//     ${moment().format('MMMM Do YYYY')}
+// `)
 
 
-$('#9').val(localStorage.getItem('9'))
-$('#10').val(localStorage.getItem('10'))
-$('#11').val(localStorage.getItem('11'))
-$('#12').val(localStorage.getItem('12'))
-$('#13').val(localStorage.getItem('13'))
-$('#14').val(localStorage.getItem('14'))
-$('#15').val(localStorage.getItem('15'))
-$('#16').val(localStorage.getItem('16'))
-$('#17').val(localStorage.getItem('17'))
+// $('#9').val(localStorage.getItem('9'))
+// $('#10').val(localStorage.getItem('10'))
+// $('#11').val(localStorage.getItem('11'))
+// $('#12').val(localStorage.getItem('12'))
+// $('#13').val(localStorage.getItem('13'))
+// $('#14').val(localStorage.getItem('14'))
+// $('#15').val(localStorage.getItem('15'))
+// $('#16').val(localStorage.getItem('16'))
+// $('#17').val(localStorage.getItem('17'))
 
 
 
-var timeNow = parseInt(moment().format('HH'));
+// let timeNow = parseInt(moment().format('HH'));
 
-function checkTime () {
-    for (i-9; i <= 17; i ++) {
-        if (parseInt($(`#${i}`).attr('id')) < timeNow) {
-            $(`#${i}`).removeClass('past present future')
-            $(`#${i}`).addClass('past')
-        } else if (parseInt($(`#${i}`).attr('i')) === timeNow) {
-            $(`#${i}`).removeClass('past present future')
-            $(`#${i}`).addClass('present')
+// function checkTime () {
+//     for (i-9; i <= 17; i ++) {
+//         if (parseInt($(`#${i}`).attr('id')) < timeNow) {
+//             $(`#${i}`).removeClass('past present future')
+//             $(`#${i}`).addClass('past')
+//         } else if (parseInt($(`#${i}`).attr('i')) === timeNow) {
+//             $(`#${i}`).removeClass('past present future')
+//             $(`#${i}`).addClass('present')
+//         } else {
+//             $(`#${i}`).removeClass('past present future')
+//             $(`#${i}`).addClass('future')
+//         }
+//     }
+// }
+
+// checkTime = {}
+
+
+// $('.saveBtn').click(function() {
+//     // target the text area using sibbling
+//         let value  = $(this).siblings(`.description`).val()
+//         let time   = $(this).siblings(`.description`).attr('id')
+//         localStorage.setItem(time,value)
+// }
+
+// )
+
+$(document).ready(function (){
+
+    $(".saveBtn").click(function() {
+        let value = $(this).siblings(".description").val()
+        console.log(value)
+        let time = $(this).parents().attr("id")
+        console.log(time)
+
+        localStorage.setItem(time, value)
+    })
+
+    $("#9").val(localStorage.getItem("9"))
+    $("#10").val(localStorage.getItem("10"))
+    $("#11").val(localStorage.getItem("11"))
+    $("#12").val(localStorage.getItem("12"))
+    $("#13").val(localStorage.getItem("13"))
+    $("#14").val(localStorage.getItem("14"))
+    $("#15").val(localStorage.getItem("15"))
+    $("#16").val(localStorage.getItem("16"))
+    $("#17").val(localStorage.getItem("17"))
+
+    let currentHour = parseInt(moment().format("H"))
+
+    $(".time-block").each(function () {
+       let timeBlock = parseInt($(this).attr("id"))
+        if(timeBlock === currentHour){
+            $(this).addClass("present")
+        } else if (timeBlock > currentHour) {
+            $(this).addClass("future")
         } else {
-            $(`#${i}`).removeClass('past present future')
-            $(`#${i}`).addClass('future')
+            $(this).addClass("past")
         }
-    }
-}
 
-
-$('.saveBtn').click(function() {
-    // target the text area using sibbling
-        let     = $(this).siblings(`.description`).val()
-        let     = $(this).siblings(`.description`).attr('id')
-        localStorage.setItem(num, hello)
-}
-
-)
-
+    })
+})
